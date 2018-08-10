dfe9ef33133067392264033fdba3fb22696360a6
Alexandre Guerra <agmguerra@gmail.com>
2018-08-10 16:05:30 -0300

    ajustes

---
+++ projeto-3/src/tree/BinaryTree.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class BinaryTree {
+	
+	class Node {
+		int value;
+		Node leftChild = null;
+		Node rightCild = null;
+		Node(int value) {
+			this.value = value;
+		}
+	}
+
+}
