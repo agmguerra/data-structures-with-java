79d8770a72b30ea4aaa48dc0e9f89d106d74eadd
Alexandre Guerra <agmguerra@gmail.com>
2018-08-10 17:16:56 -0300

    ajustes

--- projeto-3/src/tree/BinaryTree.java
+++ projeto-3/src/tree/BinaryTree.java
@@ -10,5 +10,19 @@ public class BinaryTree {
 			this.value = value;
 		}
 	}
+	
+	protected Node root = null;
+	
+	public void inOrderTraversal(Node n) {
+		if (n == null) return;
+		inOrderTraversal(n.leftChild);
+		visit(n);
+		inOrderTraversal(n.rightCild);
+	}
+	
+	public void visit(Node n) {
+		System.out.println(n.value);
+	}
 
+	
 }
