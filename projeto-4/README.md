c32b3b7b1dee5342ffe797be0220a8b2fb7ce538
Alexandre Guerra <agmguerra@gmail.com>
2018-08-17 16:50:59 -0300

    heap

---
+++ projeto-4/README.md
@@ -0,0 +1,10 @@
+# **Observações**
+
+* Max Heap é uma arvore binária na qual o nó de root é maior que seus descendentes. 
+
+* Assim o maior valor da árvore está sempre no nó root.
+* **Achar então o maior valor é sempre O(1)**
+* **Adicionar um elemento é sempre O(log n)**
+* **Remover o maior elemento é O(1) e reestruturar a árvore é O(log n)**
+
+
--- projeto-4/src/heap/Heap.java
+++ projeto-4/src/heap/Heap.java
@@ -14,4 +14,48 @@ public class Heap {
 		values[index1] = values[index2];
 		values[index2] = temp;
 	}
+	
+	
+	public void bubbleUp(int index) {
+		if (index == 0) return ;
+		int parentIndex = (index - 1)/2;
+		if (values[parentIndex] < values[index]) {
+			swapValues(index, parentIndex);
+			bubbleUp(parentIndex);
+		}
+			
+	}
+	
+	
+	public void add(int value) throws Exception {
+		if (size == values.length) {
+			throw new Exception();
+		}
+		values[size] = value;
+		bubbleUp(size);
+		size++;
+	}
+	
+	
+	public void bubbleDown(int index) {
+		int greaterChild;
+		int leftIndex = (index * 2) + 1;
+		int rightIndex = (index * 2) + 2;
+		if (rightIndex < size && values[rightIndex] > values[leftIndex]) {
+			greaterChild = leftIndex;
+		} else return ;
+		if (values[index] < values[greaterChild]) {
+			swapValues(index, greaterChild);
+			bubbleDown(greaterChild);
+		}
+	}
+	
+	public int extract() throws Exception {
+		if (size == 0) throw new Exception();
+		int extractMax = values[0];
+		values[0] = values[size - 1];
+		bubbleDown(0);
+		size -= 1;
+		return extractMax;
+	}
 }
